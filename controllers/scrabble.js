a4f9ae991df35c96fdb6716bb8c10739685cb2ef
Suzanne Harrison <harrison_family@hotmail.com>
2016-06-13 22:36:48 -0700

    most of scrabble project donw. hunting down bug for the last requirement to render score/word. rendering scored word in score/ at this time.

---
+++ controllers/scrabble.js
@@ -0,0 +1,51 @@
+var Scrabble = require('../lib/scrabble.js')
+var scrabble = new Scrabble();
+
+var ScrabbleController = {
+  index: function (req, res, next) {
+    res.render('index', { title: 'Express' })
+  },
+
+  getScrabble: function (req, res) {
+      var locals = {}
+      locals.title = "Let's play Scrabble!!!!!!!"
+      res.render('scrabble', locals)
+  },
+
+  chart: function (req, res) {
+      var locals = {}
+      locals.title = "Scrabble Letter Scores"
+      locals.letters = Scrabble.LETTER_SCORES
+      res.render('chart', locals)
+  },
+
+  // shows a form allowing a user to submit a word for scoring
+  score: function (req, res) {
+      var locals = {}
+      locals.title = "Enter Your Scrabble Word"
+      res.render('score', locals)
+  },
+
+  // posts a form with users word for scoring
+  getScore: function (req, res) {
+    var locals = {}
+    // .body from user input
+    var word = req.body.word
+    locals.title = "Word Score"
+    locals.word = word
+    locals.score = scrabble.wordScore(word)
+    res.render('display_score', locals)
+  },
+
+  displayWordScore: function (req, res) {
+    var locals = {}
+    // params from form
+    var word = req.params.word
+    locals.title = "Your Scrabble Word Score"
+    locals.word = word
+    locals.score = scrabble.wordScore(word)
+    res.render('display_score', locals)
+  }
+}
+
+module.exports = ScrabbleController
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,22 @@
 var express = require('express');
 var router = express.Router();
+var Controller = require('../controllers/scrabble');
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+router.get('/', Controller.index)
 
-module.exports = router;
+router.get('/scrabble', Controller.getScrabble)
+
+/* GET chart page. */
+router.get('/scrabble/chart', Controller.chart)
+
+/* GET form -score. */
+router.get('/scrabble/score', Controller.score)
+
+/* POST form -score. */
+router.post('/scrabble/score', Controller.getScore)
+
+/* GET scored word page. */
+router.get('/scrabble/score/:word', Controller.displayWordScore)
+
+module.exports = router
